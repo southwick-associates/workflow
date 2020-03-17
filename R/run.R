508bb6142d03898a76fe1640b5e6b5051dd9fd8d
Daniel Kary <dankary@gmail.com>
2020-03-17 13:07:51 -0400

    updated run functions to work with rmd

--- R/run.R
+++ R/run.R
@@ -1,22 +1,32 @@
 # functions to run scripts
 
-#' Source an R script, storing rendered output as .md in a log subfolder
+#' Source an R script, storing rendered output as md or html in a log subfolder
 #' 
-#' Uses \code{\link[rmarkdown]{render}} to create a log of the sourced script.
-#' The markdown output is formatted in a way that works well for github. If not
-#' using github, you may want to use \code{\link{run_html}} instead.
+#' Uses \code{\link[rmarkdown]{render}} to create a log of the sourced script:
+#' \itemize{
+#'   \item Use run() with .R files to format the log output in a way that works 
+#'   well for github (markdown)
+#'   \item Use run_html() with .R files for a log output formatted as html, which
+#'   works better for viewing within Rstudio
+#'   \item Use run_rmd() for .Rmd files to markdown
+#'   \item Use run_rmd_html() for .Rmd files to html
+#' }
 #' 
 #' @param script_path file path of R script to be executed
+#' @inheritParams rmarkdown::render
 #' @family functions to run scripts
 #' @export
 #' @examples 
 #' # workflow::run("path/to/script.R")
-run <- function(script_path) {
+#' # workflow::run_html("path/to/script.R")
+#' # workflow::run_rmd("path/to/script.Rmd")
+#' # workflow::run_rmd_html("path/to/script.Rmd")
+run <- function(script_path, knit_root_dir = getwd()) {
     # source script & render to markdown
     rmarkdown::render(
         output_format = "github_document",
         input = script_path,
-        knit_root_dir = getwd()
+        knit_root_dir = knit_root_dir
     )
     # clean-up
     # - define file paths
@@ -44,10 +54,22 @@ run <- function(script_path) {
 
 #' @rdname run
 #' @export
-run_html <- function(script_path) {
+run_html <- function(script_path, knit_root_dir = getwd()) {
     rmarkdown::render(
         input = script_path,
         output_dir = file.path(dirname(script_path), "log"),
-        knit_root_dir = getwd()
+        knit_root_dir = knit_root_dir
     )
+}
+
+#' @rdname run
+#' @export
+run_rmd <- function(script_path, knit_root_dir = NULL) {
+    run(script_path, knit_root_dir = knit_root_dir)
+}
+
+#' @rdname run
+#' @export
+run_rmd_html <- function(script_path, knit_root_dir = NULL) {
+    run_html(script_path, knit_root_dir = knit_root_dir)
 }
