bd29bc9842d28c0460f6386c0c32b665a8a826ca
Daniel Kary <dankary@gmail.com>
2020-03-17 10:02:00 -0400

    updated run.R in template

--- inst/template-init/code/run.R
+++ inst/template-init/code/run.R
@@ -1,7 +1,10 @@
 # run analysis
 
-# example production script
+# example production script (saving md log file)
 workflow::run("code/example.R")
 
-# example summary script
-rmarkdown::render("code/example.Rmd")
+# example production script (saving html log file)
+workflow::run_html("code/example.R")
+
+# example Rmd (i.e., summary script)
+workflow::run_rmd("code/example.Rmd")
